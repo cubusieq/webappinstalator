2e4537f16b97c43c2aa952a016e027dfe88ec456
cubusieq <machnik.jakub@gmail.com>
2015-12-29 10:02:18 +0100

    Code refactor.

--- src/main/java/webappinstallator/WebAppInstallator.java
+++ src/main/java/webappinstallator/WebAppInstallator.java
@@ -26,15 +26,27 @@ import org.slf4j.LoggerFactory;
 public class WebAppInstallator {
 	static Logger logger = LoggerFactory.getLogger(WebAppInstallator.class);
 
-	public WebAppInstallator(CommandLine cl) {
-		logger.debug(cl.getOptionValue("dst"));
+	public WebAppInstallator(CommandLine cl, Options declaredOptions) {
 		boolean forceInstall = false;
-		if (cl.hasOption('f')) {
-			forceInstall = true;
+		if (cl.hasOption("i") || !cl.hasOption("dst")) {
+			this.printInfo(declaredOptions);
+		} else {
+			if (cl.hasOption('f')) {
+				forceInstall = true;
+			}
+			if (this.extractWar(cl.getOptionValue("dst"), forceInstall)) {
+				logger.info("Application installed successfully in " + cl.getOptionValue("dst"));
+			}
 		}
-		if(this.extractWar(cl.getOptionValue("dst"), forceInstall)){
-			logger.info("Application installed successfully in "+ cl.getOptionValue("dst"));
-		}
+	}
+
+	private void printInfo(Options options) {
+		logger.info("Application extract war file. Provide destination path using application options :");
+		options.getOptions().forEach((option) -> {
+			StringBuilder optionInfoStringBuilder = new StringBuilder();
+			logger.info(optionInfoStringBuilder.append("Name : ").append(option.getOpt()).append(" ,")
+					.append(option.getDescription()).append(" Is required :  ").append(option.isRequired()).toString());
+		});
 	}
 
 	private boolean extractWar(String destinationPath, boolean forceInstall) {
@@ -44,11 +56,11 @@ public class WebAppInstallator {
 			destination.toFile().mkdir();
 		}
 
-		if (destination.toFile().list().length > 0 && forceInstall == false) { //Directory not empty and flag not set.
+		if (destination.toFile().list().length > 0 && forceInstall == false) {
 			logger.info(
 					"Provided directory is not empty! Check the directory and rerun installer with flag '-f' to overwrite files.");
 			return false;
-		} else if (destination.toFile().list().length > 0 && forceInstall == true) { //Directory not empty and flag set.
+		} else if (destination.toFile().list().length > 0 && forceInstall == true) {
 			try {
 				FileUtils.cleanDirectory(destination.toFile());
 			} catch (IOException ex) {
@@ -56,43 +68,37 @@ public class WebAppInstallator {
 				logger.error(ex.getMessage(), ex);
 			}
 		}
-		
+
 		if (Files.isDirectory(destination)) {
 			File warFile = new File("target.war");
-			try {
-			
-				OutputStream outputStream = new FileOutputStream(warFile);
-				IOUtils.copy( this.getClass().getResourceAsStream("target.war"),outputStream );
-				JarFile targetWar = new JarFile(warFile);
-				Enumeration<JarEntry> enumEntries = targetWar.entries();
-			
-				while (enumEntries.hasMoreElements()) {
-					JarEntry file = (java.util.jar.JarEntry) enumEntries.nextElement();
-					logger.info(file.getName());
-					File f = new File(destinationPath + java.io.File.separator + file.getName());
-					if (file.isDirectory()) {
-						f.mkdir();
-						continue;
+			try (OutputStream outputStream = new FileOutputStream(warFile)) {
+				IOUtils.copy(this.getClass().getClassLoader().getResourceAsStream("target.war"), outputStream);
+				try (JarFile targetWar = new JarFile(warFile)) {
+					Enumeration<JarEntry> enumEntries = targetWar.entries();
+					while (enumEntries.hasMoreElements()) {
+						JarEntry file = enumEntries.nextElement();
+						logger.info(file.getName());
+						File f = new File(destinationPath + java.io.File.separator + file.getName());
+						f.getParentFile().mkdirs();	//make parent folders if necessary 
+						if (!file.isDirectory()) {
+							try (InputStream is = targetWar.getInputStream(file);
+									FileOutputStream fos = new FileOutputStream(f)) {
+								while (is.available() > 0) {
+									fos.write(is.read());
+								}
+							} catch (IOException ex) {
+								logger.error(ex.getMessage(), ex);
+							}
+						}
 					}
-					InputStream is = targetWar.getInputStream(file);
-					FileOutputStream fos = new FileOutputStream(f);
-					while (is.available() > 0) {
-						fos.write(is.read());
-					}
-					fos.close();
-					is.close();
 				}
-				targetWar.close();
-				outputStream.close();
-				warFile.delete();
-				return true;
 			} catch (IOException ex) {
 				logger.error(ex.getMessage(), ex);
 				return false;
-			}finally
-			{
+			} finally {
 				warFile.delete();
 			}
+			return true;
 		} else {
 			logger.error("Provided destination path is not available/is not a directory!");
 			return false;
@@ -103,17 +109,16 @@ public class WebAppInstallator {
 		try {
 			final CommandLineParser parser = new DefaultParser();
 			Options options = new Options();
-			Option dst = Option.builder("dst").hasArg().required().argName("dst").desc("Path where app will be extracted.")
+			Option dst = Option.builder("dst").hasArg().argName("dst").desc("Path where app will be extracted.")
 					.build();
 			Option f = Option.builder("f").hasArg(false)
-					.desc("Force unpacking. True if overwrite files in dst directory.").build();
+					.desc("Force unpacking. True if you want to overwrite files in dst directory.").build();
+			Option info = Option.builder("i").hasArg(false).desc("Prints info without executing wizard.").build();
 			options.addOption(dst);
 			options.addOption(f);
+			options.addOption(info);
 			CommandLine parsedCl = parser.parse(options, args);
-			if(parsedCl.hasOption("dst")){
-			new WebAppInstallator(parsedCl);}else{
-				logger.error("Provide option -dst which should contain path where application will be installed.");
-			}
+			new WebAppInstallator(parsedCl, options);
 		} catch (ParseException ex) {
 			logger.error(ex.getMessage(), ex);
 			logger.error("Check -dst option, and try again.");
